2a6fc7d43af177ee8673a27c7acb25748b4f89ac
Zsolt Spiegelberger <zsoltspiegelberger@gmail.com>
2020-12-17 07:13:06 +0100

    UserEntity and Repository created

---
+++ PhotoAppApiUsers/src/main/java/com/spiegelberger/phototapp/api/users/data/UserRepository.java
@@ -0,0 +1,7 @@
+package com.spiegelberger.phototapp.api.users.data;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+
+}
--- PhotoAppApiUsers/src/main/java/com/spiegelberger/phototapp/api/users/service/UserServiceImpl.java
+++ PhotoAppApiUsers/src/main/java/com/spiegelberger/phototapp/api/users/service/UserServiceImpl.java
@@ -1,12 +1,14 @@
 package com.spiegelberger.phototapp.api.users.service;
 
+import java.util.UUID;
+
 import com.spiegelberger.phototapp.api.users.shared.UserDto;
 
 public class UserServiceImpl implements UserService{
 
 	@Override
 	public UserDto createUser(UserDto userDetails) {
-		
+		userDetails.setUserId(UUID.randomUUID().toString());
 		return null;
 	}
 
