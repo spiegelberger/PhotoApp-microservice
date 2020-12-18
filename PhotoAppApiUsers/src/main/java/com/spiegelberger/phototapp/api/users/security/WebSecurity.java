0dcfa5a9ffe19bbcc6e713bd33ceb5e911b7a5ee
Zsolt Spiegelberger <zsoltspiegelberger@gmail.com>
2020-12-18 06:18:27 +0100

    Tried to restrict access to users ms to only Zuul's Ip but doesn't work, commented out, it has to be solved later!

--- PhotoAppApiUsers/src/main/java/com/spiegelberger/phototapp/api/users/security/WebSecurity.java
+++ PhotoAppApiUsers/src/main/java/com/spiegelberger/phototapp/api/users/security/WebSecurity.java
@@ -1,6 +1,8 @@
 package com.spiegelberger.phototapp.api.users.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -8,11 +10,26 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter{
+	
+	Environment env;
+
+	@Autowired
+	public WebSecurity(Environment env) {
+		this.env = env;
+	}
+
+
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
-		http.authorizeRequests().antMatchers("/users/**").permitAll();
+		http.authorizeRequests().antMatchers("/**")
+		/*
+		Only Zuul should have access but this line always ends up with "Access forbidden",
+		which is a well-known Windows problem:
+		*/
+		//.hasIpAddress(env.getProperty("gateway.ip"))
+		.permitAll();
 		http.headers().frameOptions().disable();
 	}
 }
